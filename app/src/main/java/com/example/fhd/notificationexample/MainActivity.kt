0af7aecce15bfb111d74d38b17f7bfa0cf46cf55
Firoz Hasan <diganto007@gmail.com>
2019-03-07 18:41:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fhd/notificationexample/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.fhd.notificationexample
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RemoteViews
+import kotlinx.android.synthetic.main.activity_main.*
+import android.media.RingtoneManager
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+    private val channelId = "com.example.fhd.notificationexample"
+    private val description = "Test notification"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        btn_notify.setOnClickListener {
+
+            val intent = Intent(this,MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+
+            val contentView = RemoteViews(packageName,R.layout.notification_layout)
+            contentView.setTextViewText(R.id.tv_title,"CodeAndroid")
+            contentView.setTextViewText(R.id.tv_content,"Text notification")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableLights(true)
+                notificationChannel.lightColor = Color.GREEN
+                notificationChannel.enableVibration(true)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this,channelId)
+                        .setContent(contentView)
+                        .setSmallIcon(R.drawable.ic_launcher_round)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
+                        .setContentIntent(pendingIntent)
+            }else{
+                val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                builder = Notification.Builder(this)
+                        .setContent(contentView)
+                        .setSmallIcon(R.drawable.ic_launcher_round)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.resources,R.drawable.ic_launcher))
+                        .setContentIntent(pendingIntent)
+                        .setSound(alarmSound)
+                        .setVibrate(longArrayOf(1000, 1000))
+            }
+            notificationManager.notify(1234,builder.build())
+        }
+
+    }
+}
